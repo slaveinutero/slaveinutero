c33989c5e11b9ab989cb3775703cdfa29d1e930a
slaveinutero <biyekenova@gmail.com>
2023-02-11 23:46:13 +0500

    example

---
+++ main.js
@@ -0,0 +1,18 @@
+function pow(a, b) {
+    let result = a;
+  
+    for (let i = 1; i < n; i++) {
+      result *= a;
+    }
+  
+    return result;
+  }
+  
+  let a = prompt("a?", '');
+  let b = prompt("b?", '');
+  
+  if (b < 1) {
+    alert(`Число ${b} не поддерживается, используйте натуральное число`);
+  } else {
+    alert( pow(a, b) );
+  }
